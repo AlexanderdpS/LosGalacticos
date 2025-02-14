22e59681ee11791d26536d62b084285b3cbdedac
AlexanderdpS <adominguez@senati.pe>
2025-02-13 19:22:48 -0500

    el codigo

--- Python/suma.py
+++ Python/suma.py
@@ -1,2 +1,12 @@
-nombres =["Messi", "CR7", "Paul"]
-print(*nombres)
+numer1=int(input("Ingrese primer numero: "))
+numer2=int(input("Ingrese segundo numero: "))
+
+suma=numer1+numer2
+resta=numer1-numer2
+multiplicacion=numer1*numer2
+division=numer1/numer2
+
+print("La suma es: "+suma)
+print("La resta es: "+resta)
+print("La multiplicacion: "+multiplicacion)
+print("La division: "+division)
