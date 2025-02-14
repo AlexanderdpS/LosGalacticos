e179ce60176bedb6ee42208798d1e9942c185fb5
AlexanderdpS <adominguez@senati.pe>
2025-02-13 19:36:14 -0500

    el codigo

--- Python/suma.py
+++ Python/suma.py
@@ -4,9 +4,14 @@ numer2=int(input("Ingrese segundo numero: "))
 suma=numer1+numer2
 resta=numer1-numer2
 multiplicacion=numer1*numer2
-division=numer1/numer2
+
+if numer2>0:
+    division=numer1/numer2
+    print(f"La division: {division}")
+else:
+    print("Vuelva a poner el numero no se puede imprimir division")
 
 print(f"La suma es: {suma}")
 print(f"La resta es:{resta}" )
 print(f"La multiplicacion: {multiplicacion}" )
-print(f"La division: {division}")
+
